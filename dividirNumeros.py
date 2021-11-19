2755cb119ca19a4b2f3a730ae478c44e2d5038eb
JoaoPaulo-Fernandes <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-19 09:42:55 -0300

    Create dividirNumeros.py
    Divisão de valores no Python

---
+++ dividirNumeros.py
@@ -0,0 +1,19 @@
+#Programa que divide dois números e exibe o seu valor.
+
+# Divisão com resultado não inteiro.
+print(8/2)
+
+print('------------------')
+
+# Divisão um pouco mais elaborada e não inteira.
+a = 10
+b = 2
+divisao = a / b
+print(divisao)
+
+print('-----------------')
+
+# Divisão com valor inteiro. Utiliza-se duas (//).
+print(8//4)
+
+
