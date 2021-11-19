764d05a858a67f33c5d26fc346231330782cf411
JoaoPaulo-Fernandes <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-19 09:21:48 -0300

    Create subtrairNumeros.py
    Subtração de valores no Python

---
+++ subtrairNumeros.py
@@ -0,0 +1,12 @@
+#Programa que subtrai dois números e exibe o seu valor.
+
+# Forma mais simples
+print(7-4)
+
+print('------------------')
+
+# Forma um pouco mais elaborada
+a = 9
+b = 5
+subtracao = a - b
+print(subtracao)
