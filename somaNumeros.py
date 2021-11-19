15a5b9bde979f5f8e9c26365b700df35b8adc941
JoaoPaulo-Fernandes <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-19 09:14:06 -0300

    Create somaNumeros.py
    Somando números no Python

---
+++ somaNumeros.py
@@ -0,0 +1,12 @@
+#Programa que soma dois números e exibe o seu valor.
+
+# Forma mais simples
+print(2+3)
+
+print("--------------------------------")
+
+# Forma um pouco mais elaborada.
+a = 2
+b = 3
+soma = a + b
+print(soma)
