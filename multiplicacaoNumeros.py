1e273f5298fd37a07548f98e6dd5ca0fa5249a88
JoaoPaulo-Fernandes <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-19 09:48:30 -0300

    Create multiplicacaoNumeros.py
    Multiplicação de valores no Python

---
+++ multiplicacaoNumeros.py
@@ -0,0 +1,12 @@
+#Programa que multiplica dois números e exibe o seu valor.
+
+# Forma mais simples
+print(2*3)
+
+print('-------------')
+
+# Forma um pouco mais elaborada.
+a = 5
+b = 3
+multiplicacao = a * b
+print(multiplicacao)
