0037d0859b5c0a76c13a1d5f8820311e09b2ce96
JoaoPaulo-Fernandes <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-20 10:41:29 -0300

    Update dividirNumeros.py
    Melhorando as informações do comentário

--- dividirNumeros.py
+++ dividirNumeros.py
@@ -5,7 +5,7 @@ print(8/2)
 
 print('------------------')
 
-# Divisão um pouco mais elaborada e não inteira.
+# Divisão um pouco mais elaborada e não inteira. Utilizando variáveis. (Lêr-se "a" recebe 10, "b" recebe 2 e "divisão" recebe a / b)
 a = 10
 b = 2
 divisao = a / b
