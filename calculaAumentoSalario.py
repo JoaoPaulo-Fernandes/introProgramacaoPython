d494193660ecd524b424e916038771a8c5bad66d
JoaoPaulo-Fernandes <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-20 11:26:30 -0300

    Create calculaAumentoSalario.py
    Programa que calcula o aumento de salário de R$ 750,00 em 15%

---
+++ calculaAumentoSalario.py
@@ -0,0 +1,7 @@
+#Programa que calcula o aumento de salário de um funcionário em 15%.
+
+salario = 750
+aumento = (15 * 750)/ 100
+print(aumento)
+
+# Sugestão de exercício para você leitor: Crie um programa que calcule o valor do novo salário após o aumento.
