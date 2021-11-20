3ff5eaf5473a2ad96611bbcd7572ce9000e6f6b4
JoaoPaulo-Fernandes <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-20 10:42:46 -0300

    Update multiplicacaoNumeros.py
    Melhorando as informações do comentário

--- multiplicacaoNumeros.py
+++ multiplicacaoNumeros.py
@@ -5,7 +5,7 @@ print(2*3)
 
 print('-------------')
 
-# Forma um pouco mais elaborada.
+# Forma um pouco mais elaborada.Utilizando variáveis. (Lêr-se "a" recebe 5, "b" recebe 3 e "soma" recebe a * b)
 a = 5
 b = 3
 multiplicacao = a * b
