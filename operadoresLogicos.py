c5764a2a77ed8dcabb9c1ef707692575572c3507
João Paulo <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-25 11:40:40 -0300

    Create operadoresLogicos.py
    Informações básicas sobre os operadores lógicos e a tabela verdade de cada um.

---
+++ operadoresLogicos.py
@@ -0,0 +1,33 @@
+# Operadores lógicos temos:
+# not - equivale a (não) - Precisa apenas de um operador - tornando o valor verdadeiro em falso e vice-versa;
+# or - equivale a (ou) - Só resultará em falso se seus dois operadores também forem falsos;
+# and - equivale a (e) - ; 
+
+# Exemplos (Operador not):
+#not True resulta em False;
+
+#not False resulta em True;
+
+#============================================================================================================
+
+# Exemplos (Operador or)
+	
+#True or True resulta em True;
+
+#True or False resulta em True;
+
+#False or True resulta em True;
+
+#False or False resulta em False;
+
+#===========================================================================================================
+
+# Exemplos (Operador and)
+
+#True and True resulta em True
+
+#True and False resulta em False
+
+#False and True resulta em False
+
+#False and False resulta em False
