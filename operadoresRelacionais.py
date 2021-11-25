0a466f91f04ceda7d409c32e674ae48a99c137eb
João Paulo <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-25 10:45:16 -0300

    Informações básicas sobre os operadores relacionais e alguns exemplos

---
+++ operadoresRelacionais.py
@@ -0,0 +1,25 @@
+# Esses operadores são utilizados como na matemática.
+# == equivale ao símbolo de (igualdade);
+# != equivale ao símbolo de (diferente);
+# >= siginifica maior ou igual;
+# <= significa menor ou igual;
+# significa maior;
+# siginifica menor;
+
+a = 2
+b = 2
+print(a == b) # Pergunta se o valor de "a" é o mesmo que o valor de "b"
+
+c = 3
+d = 1
+print(c != d) # Pergunta se o valor de "c" é difirente do valor de "d"
+
+e = 8
+f = 2
+print(e >= f) # Pergunta se o valor de "e" é maior ou igual ao valor de "f"
+print(e <= f) # Pergunta se o valor de "e" é menor ou igual ao valor de "f"
+
+g = 1
+h = 2
+print(g > h) # Pergunta se o valor de "g" é maior que ao valor de "h"
+print(g < h) # Pergunta se o valor de "g" é menor que ao valor de "h"
---
+++ variaveisTipoLógico.py
@@ -0,0 +1,8 @@
+# Por exemplo ao queremos armazenar um conteúdo simples como: verdadeiro ou falso, utilizamos a variável do tipo lógico ou booleana (True == verdadeiro) e (False == falso).
+# Uma observação: as letras iniciais são maiúsculas (T) e (F).
+
+resultado = True
+reprovado = False
+
+print(resultado)
+print(reprovado)
