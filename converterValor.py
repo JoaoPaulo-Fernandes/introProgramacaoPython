b0d4f20237a475e9dda7f68f2e58afb720d416b3
João Paulo <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-29 16:18:38 -0300

    Create converterValor.py
    Programa que converte um valor digitado pelo o usuário de metros para milímetros.

---
+++ converterValor.py
@@ -0,0 +1,5 @@
+# Programa que recebe um valor em metros e o converte para milímetros, depois exibe em tela.
+
+metros = int(input("Digite um valor em metros: "))
+conversor = metros * 1000
+print(f"O valor digitado equivale a:",conversor, "milímetros")
