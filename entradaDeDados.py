0af486913822445ab77c3025f5d7371f3a479326
João Paulo <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-29 15:34:54 -0300

    Create entradaDeDados.py
    Informações básicas sobre entrada de dados no Python

---
+++ entradaDeDados.py
@@ -0,0 +1,4 @@
+# Considera-se entrada de dados, a partir do momento em que o programa recebe dados ou valores por um dispositivo de entrada de dados. Veja um exemplo abaixo.
+
+nome = 'José'
+print(f'Olá, {nome}! Seja bem-vindo!')
