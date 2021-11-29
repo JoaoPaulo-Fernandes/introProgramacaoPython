6ed26538bd2f5739307d5b58ee48209d53ecf283
João Paulo <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-29 11:48:33 -0300

    Create operacaoStringConcatenacao.py
    Informações básicas sobre variáveis String e a concatenação.

---
+++ operacaoStringConcatenacao.py
@@ -0,0 +1,5 @@
+# O conteúdo de variáveis string podem ser somados ou concatenados.
+
+x = 'abc'
+y = x + 'd' # Y recebe os valores de X e depois realiza uma soma com a letra (d).
+print(y) # Retornará como resultado (abcd).
