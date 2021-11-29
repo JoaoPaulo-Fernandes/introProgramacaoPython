fc868181119ec48c6fb3db6b5fb8dc556dacdd99
João Paulo <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-29 16:08:04 -0300

    Create calculaInteiros.py
    Programa que calcula a soma de dois números inteiros inseridos pelo o usuário.

---
+++ calculaInteiros.py
@@ -0,0 +1,6 @@
+# Programa que calcula a soma de dois números inteiros inseridos pelo o usuário.
+
+num1 = int(input("Digite um número inteiro: "))
+num2 = int(input("Digite outro número inteiro: "))
+soma = num1 + num2
+print(f"A soma dos números digitados é: ",soma)
