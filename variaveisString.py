2f6a464d399d6c30f5fd6bbd995fbaf3daadc2a5
João Paulo <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-11-29 11:41:52 -0300

    Create variaveisString.py
    Informações básicas sobre variáveis String e a função len.

---
+++ variaveisString.py
@@ -0,0 +1,5 @@
+# Variáveis do tipo String armazenam cadeias de caracteres como nomes e textos em geral.
+# O tamanho de uma String pode ser obtido utilizando a função (len).
+
+nome = 'python'
+print(len(nome))
