5d6cf333304efd6edfdc99b4547df97dc6e25463
João Paulo <88796273+JoaoPaulo-Fernandes@users.noreply.github.com>
2021-12-15 09:40:55 -0300

    Create exibirNumeros.py
    Programa que imprime os números de 1 a 100. Utilizando o (while).

---
+++ exibirNumeros.py
@@ -0,0 +1,6 @@
+# Programa que exibe os números de 1 a 100. Utilizando o bloco de repetição (While)
+
+x = 1                   # X inicia com o valor de 1
+while x <= 100:         # Enquanto a condição for verdadeira, o bloco será executado
+    print(x)            # Imprime o valor de x
+    x = x + 1           # X recebe o valor de 1 e sempre realiza uma nova soma.
